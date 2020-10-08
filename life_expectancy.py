c0e6bb9c7e4ee5c2e8798cf3ee46646afe321735
NNUGNHOJ <gunn.script@gmail.com>
2020-10-08 11:44:42 +0200

    Added all files

--- life_expectancy.py
+++ life_expectancy.py
@@ -10,6 +10,11 @@ shapefile = 'ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp'
 gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry', 'CONTINENT']]
 df = pd.DataFrame(gdf)
 
+#read data on life expectancy
+df_life_exp = pd.read_csv('life_exp.csv')
+
+print(df_life_exp.columns)
+
 #create df with just the countries in africa
 list_africa = []
 for index, row in df.iterrows():
@@ -24,34 +29,39 @@ gf_africa.columns = ['country', 'country_code', 'geometry', 'continent']
 #create list of African countries
 african_countries_list = gf_africa['country'].tolist()
 
-#read data on education
-df_exp_edu = pd.read_csv('government-expenditure-education.csv')
+african_life_exp_dict = dict.fromkeys(african_countries_list, None)
 
-african_countries_edu_spending_dict = dict.fromkeys(african_countries_list, 0)
-
-for index, row in df_exp_edu.iterrows():
+for index, row in df_life_exp.iterrows():
     if row['Entity'] in african_countries_list:
-        if row['Year'] == 2014:
-            african_countries_edu_spending_dict[row['Entity']] = row['Government expenditure on education, ' 
-                                                                     'total (% of government expenditure)']
+        if row['Year'] == 2016:
+            african_life_exp_dict[row['Entity']] = row['Life expectancy at birth, total (years)']
 
-df_africa['edu_spending'] = 0
-df_africa['color'] = 0
+df_africa['life_exp'] = None
+df_africa['color'] = None
 
 for index, row in df_africa.iterrows():
-    df_africa.at[index, 'edu_spending'] = african_countries_edu_spending_dict[row['country']]
+    df_africa.at[index, 'life_exp'] = african_life_exp_dict[row['country']]
+
+max_life_exp = df_africa['life_exp'].max()
+min_life_exp = df_africa['life_exp'].min()
+
+for index, row in df_africa.iterrows():
+    if row['life_exp'] is not None:
+        df_africa.at[index, 'color'] = (row['life_exp'] / max_life_exp)
 
 print(df_africa.head(10))
 
 gf_africa = gpd.GeoDataFrame(df_africa)
 #Rename columns.
-gf_africa.columns = ['country', 'country_code', 'geometry', 'continent', 'edu_spending', 'color']
+gf_africa.columns = ['country', 'country_code', 'geometry', 'continent', 'life_exp', 'color']
+
 
 gf_africa['color_rgba'] = gf_africa.apply(
-    lambda row: to_rgba(row['color'], alpha=row['edu_spending']), axis=1)
+    lambda row: to_rgba(row['color'], alpha=row['life_exp']), axis=1)
 gf_africa.plot(color=gf_africa['color_rgba'])
 
 '''
+
 #gf_africa.plot()
 #plt.show()
 
