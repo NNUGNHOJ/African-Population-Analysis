8db41404db3c16d317d4606cdfd5e5fd38e5e08a
John <gunn.script@gmail.com>
2020-10-09 18:27:35 +0200

    mmm

--- life_expectancy.py
+++ life_expectancy.py
@@ -1,6 +1,9 @@
 import geopandas as gpd
 import pandas as pd
+import seaborn as sns
+import pysal as ps
 from matplotlib.colors import to_rgba
+from matplotlib import cm
 import collections
 import matplotlib
 import matplotlib.pyplot as plt
@@ -10,10 +13,10 @@ shapefile = 'ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp'
 gdf = gpd.read_file(shapefile)[['ADMIN', 'ADM0_A3', 'geometry', 'CONTINENT']]
 df = pd.DataFrame(gdf)
 
+
+'''
 #read data on life expectancy
 df_life_exp = pd.read_csv('life_exp.csv')
-
-print(df_life_exp.columns)
 
 #create df with just the countries in africa
 list_africa = []
@@ -27,20 +30,21 @@ gf_africa = gpd.GeoDataFrame(df_africa)
 #Rename columns.
 gf_africa.columns = ['country', 'country_code', 'geometry', 'continent']
 #create list of African countries
-african_countries_list = gf_africa['country'].tolist()
+african_countries_list = gf_africa['country_code'].tolist()
 
 african_life_exp_dict = dict.fromkeys(african_countries_list, None)
 
 for index, row in df_life_exp.iterrows():
-    if row['Entity'] in african_countries_list:
+    if row['Code'] in african_countries_list:
         if row['Year'] == 2016:
-            african_life_exp_dict[row['Entity']] = row['Life expectancy at birth, total (years)']
+            african_life_exp_dict[row['Code']] = row['Life expectancy at birth, total (years)']
+
 
 df_africa['life_exp'] = None
 df_africa['color'] = None
 
 for index, row in df_africa.iterrows():
-    df_africa.at[index, 'life_exp'] = african_life_exp_dict[row['country']]
+    df_africa.at[index, 'life_exp'] = african_life_exp_dict[row['country_code']]
 
 max_life_exp = df_africa['life_exp'].max()
 min_life_exp = df_africa['life_exp'].min()
@@ -48,25 +52,53 @@ min_life_exp = df_africa['life_exp'].min()
 for index, row in df_africa.iterrows():
     if row['life_exp'] is not None:
         df_africa.at[index, 'color'] = (row['life_exp'] / max_life_exp)
+    else:
+        df_africa.at[index, 'color'] = 0
 
-print(df_africa.head(10))
 
 gf_africa = gpd.GeoDataFrame(df_africa)
+
 #Rename columns.
 gf_africa.columns = ['country', 'country_code', 'geometry', 'continent', 'life_exp', 'color']
 
-
-gf_africa['color_rgba'] = gf_africa.apply(
-    lambda row: to_rgba(row['color'], alpha=row['life_exp']), axis=1)
-gf_africa.plot(color=gf_africa['color_rgba'])
-
-'''
-
-#gf_africa.plot()
+#gf_africa.plot(column='color', cmap='Wistia')
 #plt.show()
 
-gdf['color_rgba'] = gdf.apply(
-    lambda row: to_rgba(row['color'], alpha=row['margin']), axis=1)
-gdf.plot(color=gdf['color_rgba'])
+#turn geodataframe into shapefile
+#gf_africa.to_file("result2.shp")
 
 '''
+
+#Contiguity weights matrix using queen contiguity
+w_queen = ps.queen_from_shapefile(shapefile, idVariable='ADM0_A3')
+
+weight_between_NAM_and_ZAF = w_queen['NAM']['ZAF']
+all_neighbours_of_NAM = w_queen['NAM']
+
+#print connects countries of specific country
+'''
+gdf = gdf.set_index('ADM0_A3')
+# Setup figure
+f, ax = plt.subplots(1, figsize=(6, 6))
+# Plot base layer of polygons
+gdf.plot(ax=ax, facecolor='k', linewidth=0.1)
+
+# NOTE we pass both the area code and the column name
+#      (`geometry`) within brackets!!!
+focus = gdf.loc[['NAM'], ['geometry']]
+# Plot focal polygon
+focus.plot(facecolor='red', alpha=1, linewidth=0, ax=ax)
+# Plot neighbors
+neis = gdf.loc[w_queen['NAM'], :]
+neis.plot(ax=ax, facecolor='orange', linewidth=0)
+# Title
+f.suptitle("Queen neighbors of `NAM`")
+# Style and display on screen
+plt.show()
+'''
+
+north_african_countries = ['DZA', 'EGY', 'LBY', 'SDN', 'TUN']
+west_africa_countries = ['BEN', 'BFA', 'CPV', 'CIV', 'GMB', 'GHA', 'GNB',
+                         'LBR', 'MLI', 'MRT', 'NER', 'NGA', 'SEN', 'SLE', 'TGO']
+
+#block weights
